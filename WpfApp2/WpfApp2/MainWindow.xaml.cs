8d634cf5c7e99587671f901f8a492ac88cf4fe9d
hulkiedo19 <artemmironovhaker@gmail.com>
2022-03-15 17:55:54 +0300

    second commit

---
+++ WpfApp2/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.Entity;
+using WpfApp2.Models;
+
+namespace WpfApp2
+{
+    public partial class MainWindow : Window
+    {
+        CategoryContext db;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            db = new CategoryContext();
+            db.Categories.Load();
+            categoryGrid.ItemsSource = db.Categories.Local.ToBindingList();// устанавливаем привязку к кэшу
+
+            this.Closing += MainWindow_Closing;
+        }
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            db.Dispose();
+        }
+
+        private void updateButton_Click(object sender, RoutedEventArgs e)
+        {
+            db.SaveChanges();
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(categoryGrid.SelectedItems.Count > 0)
+            {
+                for(int i = 0; i < categoryGrid.SelectedItems.Count; i++)
+                {
+                    Category category = categoryGrid.SelectedItems[i] as Category;
+                    if(category != null)
+                    {
+                        db.Categories.Remove(category);
+                    }
+                }
+            }
+            db.SaveChanges();
+        }
+    }
+}
---
+++ WpfApp2/WpfApp2/Models/CategoryContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace WpfApp2.Models
+{
+    public class CategoryContext : DbContext
+    {
+        public CategoryContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
